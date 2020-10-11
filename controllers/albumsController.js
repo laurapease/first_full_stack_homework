a377aa659267b11866b2c362985b80f68aa0589f
laurapease <laurapeasetm@gmail.com>
2020-10-11 18:59:42 -0500

    Connected new route

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -1,27 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+//Albums Model
+
+const albums = require('../models/Album');
+
+
+// NEW ROUTE
+
+router.get('/newAlbum', (req, res) => {
+    res.render('albums/newAlbum');
+});
 
 
 //INDEX ROUTE
 router.get('/', (req, res) => {
-    console.log(albums);
-    res.render('indexAlbum');
+    res.render('albums/indexAlbum', {
+        albums: albums,
+    });
 });
 
 //SHOW ROUTE
 
 router.get('/:albumIndex', (req, res) => {
     const albumIndex = req.params.albumIndex;
+    const album = albums[albumIndex];
 
-    res.render('albums/showAlbum');
+    res.render('albums/showAlbum', {
+    album: album,
+    });
 
-    // if (albums[albumIndex]) {
-    //     res.send(albums[albumIndex]);
-    // } else {
-    //     res.send('Sorry, that album deos not exist');
-    // }
 });
+
+
 
 module.exports = router;
 
